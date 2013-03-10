6e1807c7db8aa4bf78705fec6ebedeee4266e3c8
Kjetil Valle <kjetil.valle@gmail.com>
2013-03-10 13:49:19 +0100

    simple templating for posts

--- sync.js
+++ sync.js
@@ -1,7 +1,8 @@
 var fs = require('fs');
 var marked = require('marked');
 var path = require('path');
-var yaml_front = require('yaml-front-matter')
+var yaml_front = require('yaml-front-matter');
+var _ = require('underscore');
 
 
 var published_path = './posts/published';
@@ -19,9 +20,11 @@ function process_file(name) {
 	console.log("processing file %s", name);
 	fs.readFile(published_path+'/'+name, 'utf-8', function(err, data) {
 		var markdown = get_markdown(data);
+		var compiled = _.template("<html><head><title>Generated from md by js</title></head><body><%= markdown %></body></html>");
+		var content = compiled({markdown : markdown});
 		var html_file = path.basename(name, '.md')+'.html';
 		var html_path = public_path+'/'+html_file;
-		fs.writeFile(html_path, markdown, function(err) {
+		fs.writeFile(html_path, content, function(err) {
 			if (err) throw err;
   			console.log('Saved %s', html_path);
 		});
