d345a0b33e1f9d8ad5052ce352f4cadaba65d9a0
Kjetil Valle <kjetil.valle@gmail.com>
2013-03-15 23:57:00 +0100

    removed server-script

--- server.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-var connect = require("connect");
-
-var port = 8080
-
-var app = connect.createServer().use(connect.static(__dirname + '/public'));
-app.listen(port);
-console.log("Listening on http://localhost:%s/", port);
