250059115a0c8379e75245154ad564ba5fea1a65
Kjetil Valle <kjetil.valle@gmail.com>
2013-06-23 20:27:47 +0200

    made deploy remove old files

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 ./generate.js
-rsync -avz -e ssh public/ kjetil@kjetilvalle.com:/home/kjetil/web/blog/
+rsync -avz --delete -e ssh public/ kjetil@kjetilvalle.com:/home/kjetil/web/blog/
